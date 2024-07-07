a410db1bda9e763cc74753508e07451b7f5c769d
Abdulhameed <busarioyewole@gmail.com>
2024-07-07 19:07:20 +0100

    add components

---
+++ src/components/shared/ParagraphHeading.tsx
@@ -0,0 +1,7 @@
+const ParagraphHeading = ({ heading }: { heading: string }) => {
+  return (
+    <p className="text-[21px] text-grap-600 mb-4 font-semibold">{heading}</p>
+  );
+};
+
+export default ParagraphHeading;
